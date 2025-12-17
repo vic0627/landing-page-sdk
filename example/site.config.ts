14e506366134e1bbe53ef93f253868c0cd9f9d0c
victor_xu <victor_xu@auer.com.tw>
2025-12-17 15:23:28 +0800

    update: examples

--- example/site.config.ts
+++
@@ -1,3 +0,0 @@
-import type { SiteConfig } from '@landing-page-sdk/types';
-
-export default {} satisfies SiteConfig;
---
+++ examples/vanilla/site.config.ts
@@ -0,0 +1,7 @@
+import type { SiteConfig } from '@landing-page-sdk/types';
+
+export default {
+  output: {
+    assetsResolution: 'rel'
+  }
+} satisfies SiteConfig;
