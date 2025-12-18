7b6331b2f4cda8aa89ed31adca99b7a7ee973ed0
victor_xu <victor_xu@auer.com.tw>
2025-12-18 13:50:02 +0800

    update: vanilla example

--- examples/vanilla/site.config.ts
+++ examples/vanilla/site.config.ts
@@ -1,6 +1,16 @@
 import type { SiteConfig } from '@landing-page-sdk/types';
 
 export default {
+  route: {
+    hidden: [
+      {
+        route: '/about',
+        site: 'bar',
+        lang: 'en',
+        reason: '因為愛'
+      }
+    ]
+  },
   output: {
     assetsResolution: 'rel',
   },
